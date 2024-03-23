f4c8a81f17ff29364172db563877e9df7a252eaa
Ramchandar.S <139736735+ramchandar-mca2023@users.noreply.github.com>
2024-03-23 10:54:47 +0530

    Add files via upload

---
+++ src/home.js
@@ -0,0 +1,15 @@
+export default function Home(){
+    return(
+        <>
+        <div id="home">
+         <h3>Welcome to <span id="span-1">MCA</span>-<span id="span-2">BANK</span></h3>
+         <h4>"Online banking"</h4>
+         <p> Available 24/7 from almost any device with an internet connection</p>
+          <p>Create new Account ,balance checks and deposit your money.</p>
+
+        </div>
+        
+       
+        </>
+    )
+}
